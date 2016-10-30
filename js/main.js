97d8912dccd41b402798c7cc8ede16daec00935e
abrilnastassja13 <abril.saravia13@gmail.com>
2016-10-29 23:28:28 -0300

    bicis locas le faltan ultimos dos campos

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function validateForm(){
+	/* Escribe tú código aquí */
+
+//Campos obligatorios
+nombre = document.getElementById("name").value;
+if (nombre == null || nombre.length == 0) {
+	alert("Ingresa nombre");
+	return false;
+}
+
+apellido = document.getElementById("apellido").value;
+if (apellido == null || apellido.length == 0) {
+	alert("Ingresa apellido");
+	return false;
+}
+
+//Solo letras de la A-Z
+soloLetrasNombre = document.getElementById("name").value;
+if (/[0-9]/.test(soloLetrasNombre)) {
+	alert("Ingresa solo letras");
+	return false;
+}
+
+soloLetrasApellido = document.getElementById("lastname").value;
+if (/[0-9]/.test(soloLetrasApellido)) {
+	alert("Ingresa solo letras");
+	return false;
+}
+
+//Primera letra mayuscula
+nombreMayuscula = nombre.charAt(0);
+if (!(/[A-Z]/.test(nombreMayuscula))) {
+	alert("Ingresa mayucula en la primera letra");
+	return false;
+}
+
+apellidoMayuscula = apellido.charAt(0);
+if (!(/[A-Z]/.test(apellidoMayuscula))) {
+	alert("Ingresa mayucula en la primera letra");
+	return false;
+}
+
+//Email valido
+email document.getElementById("input-email").value;
+if (!(/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(email))) {
+alert("Ingresa email valido");
+return false;
+}
+
+
+
+
+}
+
